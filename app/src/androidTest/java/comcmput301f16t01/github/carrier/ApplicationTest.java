c5a5fbefe3678ec0d1aadcb417944569e1ee21b4
Hreherch <hreherch@ualberta.ca>
2016-10-07 15:27:28 -0600

    Added skeletal classes and TODOs
    Note TODOs are to be considered after project part 2.
    Added Classes:
    Driver
    FareCalculator
    Location
    Request
    Rider
    User

---
+++ app/src/androidTest/java/comcmput301f16t01/github/carrier/ApplicationTest.java
@@ -0,0 +1,13 @@
+package comcmput301f16t01.github.carrier;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
