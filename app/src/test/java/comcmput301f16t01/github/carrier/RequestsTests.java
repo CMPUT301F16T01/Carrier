2ac8ef930a5f828bf387b0d4cd4bdbc5402c6740
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2016-10-07 18:46:16 -0600

    Created a java file for RequestsTests.

---
+++ app/src/test/java/comcmput301f16t01/github/carrier/RequestsTests.java
@@ -0,0 +1,12 @@
+package comcmput301f16t01.github.carrier;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by kiete on 10/7/2016.
+ */
+
+public class RequestsTests extends TestCase {
+    //US 01.01.01 As a rider, I want to request rides between two locations
+
+}
