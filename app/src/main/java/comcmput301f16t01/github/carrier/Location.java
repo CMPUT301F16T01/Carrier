d51b6116382bbb09b542600c80f520b816a6499a
sehoven <shoven@ualberta.ca>
2016-10-10 15:41:42 -0600

    Added location tests

--- app/src/main/java/comcmput301f16t01/github/carrier/Location.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/Location.java
@@ -4,5 +4,26 @@ package comcmput301f16t01.github.carrier;
  * Represents a geo-location on the globe.
  */
 public class Location {
+    private double latitude;
+    private double longitude;
 
+    public Location() {
+        // get current location
+    }
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void setLocation(double latitude, double longitude) {
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
 }
