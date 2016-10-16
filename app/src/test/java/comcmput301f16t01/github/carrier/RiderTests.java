4298123d97d5d0c3338c2d72bdbc9f7e03f5976a
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2016-10-16 17:04:46 -0600

    The tests pass and this works but there is the problem of having to update in two spots. Gonna make a new branch.

--- app/src/test/java/comcmput301f16t01/github/carrier/RiderTests.java
+++ app/src/test/java/comcmput301f16t01/github/carrier/RiderTests.java
@@ -3,7 +3,10 @@ package comcmput301f16t01.github.carrier;
 import org.junit.After;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by kiete on 10/16/2016.
@@ -39,11 +42,19 @@ public class RiderTests {
         String password = "bennettIsBae123";
         String email = "kieter@kieter.me";
         String phoneNumber = "7801234567";
-        Rider kieter = new Rider(name, password, email, phoneNumber);
+        Rider kieterRider = new Rider(name, password, email, phoneNumber);
+        RiderList riderList = new RiderList();
+
+        // TODO should really be using something called addRider but that hasn't been made yet.
+        riderList.add(kieterRider);
+        assertTrue("The list does not contain kieter", riderList.contains(kieterRider));
+
+        //TODO should really be using the controller to add stuff but that hasn't been implemented.
         Boolean authenticated = false;
 
-        //TODO finish this test.
-        //authenticated =
+        uc.getUsersHashMap().put(name, kieterRider);
+        authenticated = uc.authenticate(name, password);
+        assertTrue("Was not authenticated but credentials were right.", authenticated);
 
     }
 
