6efc3c3e984c5f79fef48aa9b42a308ce723f882
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2016-10-15 19:57:12 -0600

    Made a new folder in res called anim, it contains animations. I also made a shake animation and now the error message in activity_login.xml shakes when you get your username and password wrong.

--- app/src/main/java/comcmput301f16t01/github/carrier/LoginActivity.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/LoginActivity.java
@@ -1,8 +1,14 @@
 package comcmput301f16t01.github.carrier;
 
+import android.app.Dialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * LoginActivity is where the user enters a username and password so they can access their account.
@@ -16,6 +22,7 @@ TODO javadoc this
 TODO comment this
  */
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +38,15 @@ public class LoginActivity extends AppCompatActivity {
         //TODO jarble this?
         String passwordString = passwordEditText.getText().toString();
 
+        // Attempt to authenticate credentials and "log in"
         try {
             uc.authenticate();
         } catch (AuthenticationException authException){
-            
+            // Show the LoginErrorTextView
+            TextView loginErrorTextView = (TextView) findViewById(R.id.LoginErrorTextView);
+            loginErrorTextView.setVisibility(View.VISIBLE);
+            // Shake it!
+            loginErrorTextView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake));
         }
 
     }
