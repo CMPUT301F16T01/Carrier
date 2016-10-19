e5d15f82674b9053a2a3e0025dbe364bfba61e20
Hreherch <hreherch@ualberta.ca>
2016-10-18 23:53:23 -0600

    Added Log Out functionality to MainActivity

--- app/src/main/java/comcmput301f16t01/github/carrier/MainActivity.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/MainActivity.java
@@ -1,9 +1,13 @@
 package comcmput301f16t01.github.carrier;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.FloatRange;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -76,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
                 // do nothing
             }
         };
-
         tabLayout.addOnTabSelectedListener(onTabSelectedListener);
 
         ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
@@ -94,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-
         mViewPager.addOnPageChangeListener(onPageChangeListener);
 
 
@@ -135,12 +137,41 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_viewProfile) {
+            Toast.makeText( MainActivity.this, "Wanna view your profile? Nope!",
+                    Toast.LENGTH_SHORT ).show();
+            // TODO link to user profile.
+        }
+
+        if (id == R.id.action_logOut ) {
+            onBackPressed();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * When back is pressed or the "Log Out" menu option is selected:
+     *      Pop up a AlertDialog to confirm and open a new LoginActivity, while closing the current
+     *      RiderMainActivity.
+     */
+    public void onBackPressed() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("Are you sure?");
+        adb.setMessage("Log out and return to the login screen?");
+        adb.setCancelable(true);
+        final Activity activity = MainActivity.this;
+        adb.setPositiveButton("Log Out", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finish();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                // TODO log out current user? (UserController) 
+            }
+        });
+        adb.setNegativeButton("Cancel", null );
+        adb.show();
     }
 
     public void startMakeRequestActivity(View view) {
