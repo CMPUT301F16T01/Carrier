cf80d798f7f2c29a7645ac336fceb5db66051a9f
MichalParadis <paradis2@ualberta.ca>
2016-11-08 19:25:01 -0700

    Initialize values in the driver view request activity

--- app/src/main/java/comcmput301f16t01/github/carrier/DriverViewRequestActivity.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/DriverViewRequestActivity.java
@@ -2,15 +2,116 @@ package comcmput301f16t01.github.carrier;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * This will help us show the request from the perspective of a driver
+ * Will have the position in the requestcontroller bundled to determine what request to display.
  */
 public class DriverViewRequestActivity extends AppCompatActivity {
+    private Integer position = 0;
+    private Request request;
+    private User loggedInUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_view_request);
+        //getting the request controller to get a list of requests
+        RequestController rc = new RequestController();
+        loggedInUser = UserController.getLoggedInUser();
+        ArrayList<Request> requestList = RequestController.getInstance();
+
+        // unpacking the bundle to get the position of request
+        Bundle bundle = getIntent().getExtras();
+        position = bundle.getInt("position");
+        request = requestList.get(position);
+        if (request != null && loggedInUser != null) {
+            getViewIds();
+            setViewIds();
+        }
+
+        /**
+         * This switch statement changes the status image
+         * There is currently coloring code in here which we may use
+         * in the future for a notification system?
+         */
+        ImageView statusImageView = (ImageView) findViewById(R.id.imageView_requestStatus);
+        if (statusImageView != null) {
+            switch (request.getStatus()) {
+                case (Request.OPEN):
+                    statusImageView.setImageResource(R.drawable.open);
+                    //statusImageView.setBackgroundResource(R.color.openStatus);
+                    break;
+                case (Request.OFFERED):
+                    statusImageView.setImageResource(R.drawable.offered);
+                    //statusImageView.setBackgroundResource(R.color.offeredStatus);
+                    break;
+                case (Request.CONFIRMED):
+                    statusImageView.setImageResource(R.drawable.confirmed);
+                    //statusImageView.setBackgroundResource(R.color.confirmedStatus);
+                    break;
+                case (Request.COMPLETE):
+                    statusImageView.setImageResource(R.drawable.complete);
+                    //statusImageView.setBackgroundResource(R.color.completeStatus);
+                    break;
+                case (Request.PAID):
+                    statusImageView.setImageResource(R.drawable.paid);
+                    //statusImageView.setBackgroundResource(R.color.paidStatus);
+                    break;
+                case (Request.CANCELLED):
+                    statusImageView.setImageResource(R.drawable.cancel);
+                    //statusImageView.setBackgroundResource(R.color.cancelledStatus);
+                    break;
+
+            }
+        }
+    }
+
+    private TextView fareAmountTextView;
+    private TextView startLocationTextView;
+    private TextView descriptionTextView;
+    private TextView riderTextView;
+    private TextView driverTextView;
+
+    /**
+     * Will set the various views to display the correct information for the
+     * request
+     */
+    private void setViewIds() {
+        fareAmountTextView.setText(new MakeRequestActivity().formatFare(request.getFare()));
+        startLocationTextView.setText(request.getStart().toString());
+        descriptionTextView.setText((request.getDescription()));
+        riderTextView.setText(request.getRider().getUsername());
+        if (request.getConfirmedDriver() != null) {
+            driverTextView.setText(request.getConfirmedDriver().getUsername());
+        }
+        else {
+            driverTextView.setText("");
+        }
+    }
+
+    /**
+     * Will get the ViewIds from the R.java file.
+     */
+    private void getViewIds() {
+        fareAmountTextView = (TextView) findViewById(R.id.textView_$fareAmount);
+        startLocationTextView = (TextView) findViewById(R.id.textView_start);
+        descriptionTextView = (TextView) findViewById(R.id.textView_description);
+        riderTextView = (TextView) findViewById(R.id.textView_rider);
+        driverTextView = (TextView) findViewById(R.id.textView_driver);
+    }
+
+    public void MakeOffer(View v) {
+        RequestController rc = new RequestController();
+        if (request.getConfirmedDriver() != null) {
+
+        }
     }
 }
