887a10a11a51f0957340a65358c4e94b5cdfed2f
MichalParadis <paradis2@ualberta.ca>
2016-11-19 10:00:29 -0700

    Can confirm driver but I had a crash so I am going to try and recreate the error.

--- app/src/main/java/comcmput301f16t01/github/carrier/OfferingDriversArrayAdapter.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/OfferingDriversArrayAdapter.java
@@ -1,14 +1,22 @@
 package comcmput301f16t01.github.carrier;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import comcmput301f16t01.github.carrier.Requests.Request;
+import comcmput301f16t01.github.carrier.Requests.RequestController;
 
 /**
  * Created by michael on 17/11/16.
@@ -16,6 +24,10 @@ import java.util.ArrayList;
 
 public class OfferingDriversArrayAdapter extends ArrayAdapter<User> {
     private ArrayList<User> offeringDriversList;
+    /**
+     * Request is needed to provide a way to confirm the driver.
+     */
+    private Request request = null;
 
     public OfferingDriversArrayAdapter(Context context, int textViewResourceId, ArrayList<User> userArrayList) {
         super(context, textViewResourceId, userArrayList);
@@ -37,13 +49,47 @@ public class OfferingDriversArrayAdapter extends ArrayAdapter<User> {
         final User offeringDriver = offeringDriversList.get(position);
 
         if (offeringDriver != null) {
-            // Get the username to display it
             UsernameTextView offeringDriverUsernameTextView = (UsernameTextView) v.findViewById(R.id.UsernameTextView_offeringDriver);
             offeringDriverUsernameTextView.setText(offeringDriver.getUsername());
             offeringDriverUsernameTextView.setUser(offeringDriver);
-
         }
-        return v;
+        if (request != null) {
+            Button confirmDriverButton = (Button) v.findViewById(R.id.confirmDriverButton);
+            confirmDriverButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View view) {
+                    AlertDialog.Builder adb = new AlertDialog.Builder(view.getContext());
+                    adb.setMessage("Confirm Driver?");
+                    adb.setCancelable(true);
+                    adb.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            RequestController rc = new RequestController();
+                            rc.confirmDriver(request, offeringDriver);
+                            Toast.makeText(view.getContext(), "Confirmed Driver", Toast.LENGTH_SHORT).show();
+                            ((Activity) view.getContext()).finish(); // Finish the activity.
+                        }
+                    });
+                    adb.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    adb.show();
+                }
+            });
+        }
+
+    return v;
+}
+
+    /**
+     * Is used to set the request that this array adapter is for.
+     *
+     * @param request The request that will be used to confirm the driver.
+     */
+    public void setRequest(Request request) {
+        this.request = request;
     }
 }
 
--- app/src/main/java/comcmput301f16t01/github/carrier/RiderViewOfferingDriversActivity.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/RiderViewOfferingDriversActivity.java
@@ -1,12 +1,14 @@
 package comcmput301f16t01.github.carrier;
 
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -29,32 +31,10 @@ public class RiderViewOfferingDriversActivity extends AppCompatActivity {
         request = new Gson().fromJson( bundle.getString("request"), Request.class );
         final ArrayList<User> offeringDrivers = request.getOfferedDrivers();
         OfferingDriversArrayAdapter offeringDriversArrayAdapter = new OfferingDriversArrayAdapter(this, R.layout.offeringdriverslist_item, offeringDrivers);
+        offeringDriversArrayAdapter.setRequest(request);
         ListView offeringDriversListView = (ListView) findViewById(R.id.offeringDriversListView);
         offeringDriversListView.setAdapter(offeringDriversArrayAdapter);
-        offeringDriversListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                AlertDialog.Builder adb = new AlertDialog.Builder(RiderViewOfferingDriversActivity.this);
-                adb.setMessage("Confirm Driver?");
-                adb.setCancelable(true);
-                adb.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        RequestController rc = new RequestController();
-                        rc.confirmDriver(request, offeringDrivers.get(position));
-                        Toast.makeText(RiderViewOfferingDriversActivity.this, "Confirmed Driver", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                });
-                adb.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                });
-                adb.show();
 
-            }
-        });
     }
 }
