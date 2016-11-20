2dcf0fd2c757f977b11390641d092fabe76c3c89
Hreherch <hreherch@ualberta.ca>
2016-11-20 14:11:07 -0700

    Singleton-ified UserController
    Removed deprecated methods and modified Javadocs

--- app/src/main/java/comcmput301f16t01/github/carrier/Users/UserController.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/Users/UserController.java
@@ -1,82 +1,104 @@
 package comcmput301f16t01.github.carrier.Users;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.Patterns;
 
-import java.util.ArrayList;
-
-import comcmput301f16t01.github.carrier.LoginActivity;
-import comcmput301f16t01.github.carrier.UserList;
-
 /**
  * Singleton Pattern
- * * modifies/returns a RequestList model
- * * @see Request
- * * @see RequestList
+ * Holds an instance of an user (the user of the app), allows for logging in users with elastic
+ * search or memory.
  */
 public class UserController {
-    private static UserList userList = null;
     private static User loggedInUser = null;
 
+    private UserController() { /* prevent UserController instantiation */ }
 
-    public UserController() {
-        if (userList == null) {
-            userList = new UserList();
-        }
-    }
-
-    public static ArrayList<User> getUserList() {
-        if (userList == null) {
-            userList = new UserList();
-        }
-        return userList.getUsers();
-    }
-
-    // TODO removed the static designation so it could be accessed from MakeRequestActivity...is this okay?
     /**
-     * Will return the current logged in user.
+     * Will return the singleton instance of the current logged in user.
      *
-     * @return Returns the logged in ser
+     * @return Returns the currently logged in user
+     * @throws IllegalAccessError if there is no currently logged in user.
+     * @see #logInUser(String)
      */
     public static User getLoggedInUser() {
+        if (loggedInUser == null) {
+            throw new IllegalAccessError("You must first log in a user before getting an instance of one.");
+        }
         return loggedInUser;
     }
 
     /**
-     * Deprecated: use logInUser instead.
-     * @see #logInUser(String)
+     * Attempt to create a new user, if successful it will set the new user to the logged in user.
+     *
+     * @param username    The username that the new user will have
+     * @param email       The email the new user will have
+     * @param phoneNumber The phone number the new user will have.
+     * @return A string containing an error message if unsuccessful, otherwise null.
      */
-    @Deprecated
-    public void setLoggedInUser(User user) {
-        loggedInUser = user;
+    public static String createNewUser(@NonNull String username, @NonNull String email, @NonNull String phoneNumber) {
+        User newUser = new User();
+
+        // Trim leading and trailing whitespace
+        email = email.trim();
+        phoneNumber = phoneNumber.trim();
+        username = username.trim();
+
+        newUser.setEmail(email);
+        newUser.setPhone(phoneNumber);
+        newUser.setUsername(username);
+
+        // Ensure no entries are blank
+        if (email.equals("") || phoneNumber.equals("") || username.equals("")) {
+            return "Username, email, and phone number may not be blank.";
+        }
+
+        // Ensure a username is unique
+        if (!checkUniqueUsername(username)) {
+            return "That username is already taken!";
+        }
+
+        // Check if the email matches the email pattern and phone pattern
+        if (!validateEmail(email)) {
+            return "That doesn't look like a valid email!";
+        }
+        if (!validatePhone(phoneNumber)) {
+            return "That doesn't look like a valid phone number!";
+        }
+
+        ElasticUserController.AddUserTask aut = new ElasticUserController.AddUserTask();
+        aut.execute(newUser);
+
+        // TODO bad waiting. (isConnected here?)
+        while (newUser.getId() == null) {
+            // waiting...
+        }
+
+        loggedInUser = newUser; // set the logged in user to be the one we created.
+        return null;
     }
 
     /**
-     * Making sure that the username is unique across all riders
-     * otherwise it throws an exception
-     * <p>
-     * return True if it is unique and False if it is similar
-     *
-     * @param rider
+     * returns true if the email matches a valid email pattern.
      */
-    @Deprecated
-    public boolean uniqueRiderUsername(User rider) {
-        return false;
+    private static boolean validateEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     /**
-     * Making sure that the username is unique across all drivers
-     * otherwise it throws an exception
-     * <p>
-     * return True if it is unique and False if it is similar
-     *
-     * @param driver
+     * returns true if the phone number matches a valid phone number pattern
      */
-    @Deprecated
-    public boolean uniqueDriverUsername(User driver) {
-        return false;
+    private static boolean validatePhone(String phoneNumber) {
+        return Patterns.PHONE.matcher(phoneNumber).matches();
     }
 
+    /**
+     * Attempts to check elastic search for the passed username. Returns whether or not that username
+     * was found.
+     *
+     * @param username The username you would like to check elastic search for.
+     * @return True if that username was found, false otherwise.
+     */
     private static boolean checkUniqueUsername(String username) {
         ElasticUserController.FindUserTask fut = new ElasticUserController.FindUserTask();
         fut.execute(username);
@@ -86,19 +108,43 @@ public class UserController {
         } catch (Exception e) {
             Log.i("checkUniqueUsername", "bad error");
         }
-        if (foundUser == null) {
-            return true;
-        }
-        return false;
+        return (foundUser == null); // return the boolean result
     }
 
-    // TODO simplify above and below things (share the same code...)
+    /**
+     * Edits the logged in user given a newEmail and newPhone
+     *
+     * @param newEmail The email to change to
+     * @param newPhone The phone number to change to
+     */
+    public static void editUser(String newEmail, String newPhone) {
+        // TODO this should modify the logged in user here, but it does not?
+        ElasticUserController.EditUserTask eut = new ElasticUserController.EditUserTask();
+        eut.execute(UserController.getLoggedInUser().getId(), newEmail, newPhone);
+    }
 
     /**
-     * returns false if no user with that username, otherwise sets them as logged in
+     * Deletes a user from elastic search. (For testing only)
      *
-     * @param username
-     * @return
+     * @param usernameToDelete The username to delete from ElasticSearch
+     */
+    public static void deleteUser(String usernameToDelete) {
+        ElasticUserController.DeleteUserTask dut = new ElasticUserController.DeleteUserTask();
+        dut.execute(usernameToDelete);
+    }
+
+    /**
+     * Logs out the current user by setting them to null.
+     */
+    public void logOutUser() {
+        loggedInUser = null;
+    }
+
+    /**
+     * Attempts to log in a user and set that user to the singleton instance.
+     *
+     * @param username The username you would like to try logging in with.
+     * @return True if the user was successfully logged in, else false.
      */
     public boolean logInUser(String username) {
         ElasticUserController.FindUserTask fut = new ElasticUserController.FindUserTask();
@@ -116,121 +162,12 @@ public class UserController {
         return true;
     }
 
-    public void setEmail(User user, String email) {
-        user.setEmail(email);
-    }
-
-    public void setPhone(User user, String phone) {
-        user.setPhone(phone);
-    }
-
-    public void setUsername(User user, String username) {
-        user.setUsername(username);
-    }
-
-    @Deprecated
-    public void addDriver(User driver) {
-    }
-
-    @Deprecated
-    public void addRider(User rider) {
-    }
-
     /**
-     * authenticate is called when the user needs to login. Checks to see if the username
-     * the user entered is valid. It throws an exception when the
-     * username is wrong. Authenticate also sets the loggedInUser upon
-     * successful login.
+     * Searches elastic search for the given username and returns a User object matching that
+     * username.
      *
-     * @param usernameString The username the user attempts to login with
-     * @throws NullPointerException Happens when the user enters a username with a username that
-     *                              does not exist.
-     * @author Kieter
-     * @see LoginActivity
-     * @since Saturday October 15th, 2016
-     */
-    @Deprecated
-    //TODO re-implement after elastic search is all good
-    public boolean authenticate(String usernameString) throws NullPointerException {
-        User attemptedUser = null;
-
-        // Iterate over all the users, checking to see if the given username is the users
-        for (User user : this.getUserList()) {
-            // If there is a username match, store the user
-            if (usernameString.equals(user.getUsername())) {
-                this.loggedInUser = user;
-                return true;
-            }
-        }
-        // Otherwise this is a faulty login.
-        return false;
-    }
-
-    /**
-     * resets the UserController. Primarily used for testing.
-     *
-     */
-    public void reset() {
-        // TODO this never had the option to clear UserList.
-    }
-
-    /**
-     * Attempt to create a new user.
-     *
-     * @param username
-     * @param email
-     * @param phoneNumber
-     * @return
-     */
-    public static String createNewUser(String username, String email, String phoneNumber) {
-        // TODO testing offline behaviour
-        User newUser = new User();
-
-        // Trim leading and trailing whitespace
-        email = email.trim();
-        phoneNumber = phoneNumber.trim();
-        username = username.trim();
-
-        newUser.setEmail(email);
-        newUser.setPhone(phoneNumber);
-        newUser.setUsername(username);
-
-        // TODO more checks need to be done when adding a user, not important.
-
-        // Ensure no entries are blank
-        if( email.equals("") || phoneNumber.equals("") || username.equals("") ) {
-            return "Username, email, and phone number may not be blank.";
-        }
-
-        // Ensure a username is unique
-        if (!checkUniqueUsername(username)) {
-            return "That username is already taken!";
-        }
-
-        // Check if the email matches the email pattern
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches() ) {
-            return "That doesn't look like a valid email!";
-        }
-
-        if (!Patterns.PHONE.matcher(phoneNumber).matches()) {
-            return "That doesn't look like a valid phone number!";
-        }
-
-        ElasticUserController.AddUserTask aut = new ElasticUserController.AddUserTask();
-        aut.execute(newUser);
-
-        while (newUser.getId() == null) {
-            // waiting...
-        }
-
-        loggedInUser = newUser;
-        return null;
-    }
-
-    /**
-     * Searches elastic search for the given username and returns a User object matching that username
      * @param username The username to search for
-     * @return
+     * @return The specified User (by username)
      */
     public User findUser(String username) {
         User foundUser = null;
@@ -245,28 +182,5 @@ public class UserController {
         }
 
         return foundUser;
-    }
-
-    /**
-     * Edits the logged in user given a newEmail and newPhone
-     * @param newEmail The email to change to
-     * @param newPhone The phone number to change to
-     */
-    public static void editUser(String newEmail, String newPhone) {
-        ElasticUserController.EditUserTask eut = new ElasticUserController.EditUserTask();
-        eut.execute(UserController.getLoggedInUser().getId(), newEmail, newPhone);
-    }
-
-    /**
-     * Deletes a user from elastic search
-     * @param usernameToDelete The username to delete from ElasticSearch
-     */
-    public void deleteUser(String usernameToDelete) {
-        ElasticUserController.DeleteUserTask dut = new ElasticUserController.DeleteUserTask();
-        dut.execute(usernameToDelete);
-    }
-
-    public void logOutUser() {
-        loggedInUser = null;
     }
 }
