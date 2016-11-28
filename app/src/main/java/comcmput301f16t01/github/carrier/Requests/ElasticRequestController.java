8c146cd637386c1bafe5fa7b716ff623551d54f7
sehoven <shoven@ualberta.ca>
2016-11-28 05:43:42 -0700

    Fixed search by location issue

--- app/src/main/java/comcmput301f16t01/github/carrier/Requests/ElasticRequestController.java
+++ app/src/main/java/comcmput301f16t01/github/carrier/Requests/ElasticRequestController.java
@@ -249,9 +249,6 @@ public class ElasticRequestController {
                 e.printStackTrace();
                 Log.i("Error", "Something went wrong when we tried to talk to elastic search");
             }
-            
-            // Load all the offers from these requests
-            getOffers( foundRequests );
 
             // Filter the requests so that we can't see
             filterOutLoggedInUser( foundRequests );
